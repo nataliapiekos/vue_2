83af28807590937ee199b73fc4fcdcd489d34d2d
nataliapiekos <natalia1.piekos@gmail.com>
2020-11-13 17:41:07 +0100

    Initial Vue.js project2

--- prymus-vue/src/App.vue
+++ prymus-vue/src/App.vue
@@ -1,19 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>My vue</h1>
+    <p>Example paragraph :)</p>
+    <img src="https://picsum.photos/200/300/?random">
+    <div>
+      {{name}} {{surname}} {{age}}
+    </div>
+    <span v-bind:title="alert">This is span 1</span>
+    <br>
+    <span :title="alert">This is span 2</span>
+    <br>
+    <div> {{age}}</div>
+    <input type="text"  v-model="age">
+    <br>
+    <div v-if="age >= 18">Adult</div>
+    <div v-else>Child</div>
+    <ul id="v-for">
+      <li v-for="fruit in fruits" :key="fruit">{{ fruit }}</li>
+    </ul>
+    <div> Clicked {{counter}} times! </div>
+    <button @click="buttonClicked">Click!!</button>
+    <button v-on:click="counter = 0">Start over</button>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    data: function () {
+      return {
+        name: 'Natalia',
+        surname: 'Piękoś',
+        age: 20,
+        alert: 'This is alert',
+        fruits: [ "Banana", "Apple", "Orange"],
+        counter: 0,
+      } 
+    },
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    methods: {
+      buttonClicked: function () {
+        this.counter = this.counter + 1; 
+      }
+    }
+  };
 </script>
 
 <style>
