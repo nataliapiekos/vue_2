5d2b18ee38767d3a5c473e2e3a5eb5f813cd55c6
nataliapiekos <natalia1.piekos@gmail.com>
2020-11-13 18:45:57 +0100

    Participant list

--- prymus-vue/src/App.vue
+++ prymus-vue/src/App.vue
@@ -1,58 +1,86 @@
 <template>
-  <div>
-    <h1>My vue</h1>
-    <p>Example paragraph :)</p>
-    <img src="https://picsum.photos/200/300/?random">
-    <div>
-      {{name}} {{surname}} {{age}}
+<div id="container-fluid">
+    <nav class="navbar navbar-dark bg-dark">
+      <span class="navbar-text">
+        Participants list
+      </span>
+    </nav>
+    <div class="participant-list">
+     <h1>List</h1>
+    <div v-if="participants.length > 0">
+    <div v-for="participant in participants" :key="participant.id">
+      {{ participant.name}} {{ participant.lastName}} 
+      <button type="submit" class="btn btn-primary" v-on:click.prevent="deleteParticipant(participant)">Delete</button>
+      </div>
     </div>
-    <span v-bind:title="alert">This is span 1</span>
-    <br>
-    <span :title="alert">This is span 2</span>
-    <br>
-    <div> {{age}}</div>
-    <input type="text"  v-model="age">
-    <br>
-    <div v-if="age >= 18">Adult</div>
-    <div v-else>Child</div>
-    <ul id="v-for">
-      <li v-for="fruit in fruits" :key="fruit">{{ fruit }}</li>
-    </ul>
-    <div> Clicked {{counter}} times! </div>
-    <button @click="buttonClicked">Click!!</button>
-    <button v-on:click="counter = 0">Start over</button>
-  </div>
-  
+    <div v-else class="alert alert-dark" role="alert">
+      No participants yet!
+    </div>
+    </div>
+    <div class="participant-add-form">
+      <form>
+        <h1>Add participants</h1>
+        <div class="form-group">
+          <label for="participant-name">Name</label>
+          <input class="form-control" id="participant-name" placeholder="Enter name" v-model="newParticipantName">
+        </div>
+        <div class="form-group">
+          <label for="participant-last-name">Last name</label>
+          <input class="form-control" id="participant-last-name"  placeholder="Enter last name" v-model="newParticipantLastName">
+        </div>
+        <button type="submit" class="btn btn-primary" v-on:click.prevent="addParticipant()">Submit</button>
+      </form>
+      </div>
+</div>
+
 </template>
 
 <script>
-  export default {
-    data: function () {
-      return {
-        name: 'Natalia',
-        surname: 'Piękoś',
-        age: 20,
-        alert: 'This is alert',
-        fruits: [ "Banana", "Apple", "Orange"],
-        counter: 0,
-      } 
-    },
-
-    methods: {
-      buttonClicked: function () {
-        this.counter = this.counter + 1; 
-      }
+export default {
+  data: function (){
+    return {
+      participants: [],
+      newParticipantName: null,
+      newParticipantLastName: null,
     }
-  };
+  }, 
+  methods: {
+    addParticipant: function(){
+      var newParticipant = { name: this.newParticipantName, lastName: this.newParticipantLastName };
+    newParticipant.id = Math.random();
+      this.participants.push(newParticipant);
+      this.newParticipantName = null;
+      this.newParticipantLastName = null;
+    },
+    deleteParticipant: function(participant){
+      this.participants = this.participants.filter((added) => added.id !== participant.id);
+    }
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .container-fluid {
+    margin:0;
+    padding:0
+   }
+
+   .participant-list {
+     margin: 1rem;
+   }
+   .participant-add-form {
+     margin: 1rem;
+   }
+
+   .participant-add-form h1 {
+     background-color: #76A0B0;
+     color:white;
+     width:50%;
+     margin-left: auto; 
+     margin-right: auto;
+     text-align: center;
+     border-radius: 5px;
+   }
+
 </style>
+
--- prymus-vue/src/main.js
+++ prymus-vue/src/main.js
@@ -1,3 +1,7 @@
+
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
 import Vue from 'vue'
 import App from './App.vue'
 
